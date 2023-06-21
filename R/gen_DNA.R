373d62caf704db5ba333661d4a976dea39f42718
Paulius Baltrusis <pauliusbaltrusis@gmail.com>
2023-06-21 10:44:58 +0200

    updated documentation

--- R/gen_DNA.R
+++ R/gen_DNA.R
@@ -1,11 +1,11 @@
 #' Generate a random DNAString object of length n
 #'
-#' @param n
+#' @param n, length of the DNAString you want
 #'
-#' @return
+#' @return a DNAString object of length n
 #' @export
 #' @importFrom Biostrings DNAString
-#' @examples
+#' @examples gen_DNA(100)
 gen_DNA<- function(n){
   x<-do.call(paste0, replicate(1, sample(c('A','T','C','G'),n, TRUE), FALSE)) |>
     paste0(collapse = '') |>
