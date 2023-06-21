adb22ebb9031f30a9a99144ab0cd95bd2d26cadd
Paulius Baltrusis <pauliusbaltrusis@gmail.com>
2023-06-21 10:33:43 +0200

    Descriptions

--- R/gen_DNA.R
+++ R/gen_DNA.R
@@ -1,4 +1,11 @@
-library(Biostrings)
+#' Generate a random DNAString object of length n
+#'
+#' @param n
+#'
+#' @return
+#' @export
+#' @importFrom Biostrings DNAString
+#' @examples
 gen_DNA<- function(n){
   x<-do.call(paste0, replicate(1, sample(c('A','T','C','G'),n, TRUE), FALSE)) |>
     paste0(collapse = '') |>
